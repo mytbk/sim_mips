22f7bc7cb2b66a0641ee642de4fde3703ad0b441
Iru Cai <mytbk920423@gmail.com>
2014-11-28 23:11:41 +0800

    use macros in syscall source code

--- syscall.c
+++ syscall.c
@@ -1,21 +1,23 @@
-#include "mips.h"
-#include "mmu.h"
+#include "syscall.h"
 #include <sys/utsname.h>
 #include <string.h>
 #include <stdio.h>
 
 void sys_uname(struct mips_regs *r, struct mmu *m)
 {
-    uint32_t va = (struct utsname*)r->regs[4];
+    uint32_t va = (struct utsname*)CALLARG(r,0);
     struct utsname *name = (struct utsname*)mmu_translate_addr(m, va);
     strcpy(name->sysname, "Linux");
     strcpy(name->version, "3.10");
     strcpy(name->machine, "mipsel");
-    r->regs[7] = 0;
-    r->regs[2] = 0;
+    CALLSTATE(r) = 0;
+    CALLRET(r) = 0;
 }
 
 void sys_brk(struct mips_regs *r, struct mmu *m)
 {
-    fprintf(stderr, "sys_brk not implemented yet\n");
+    fprintf(stderr, "sys_brk: %x\n", CALLARG(r, 0));
+    
+    CALLRET(r) = mmu_set_brk(m, m->brk+CALLARG(r,0));
+    CALLSTATE(r) = 0;
 }
--- syscall.h
+++ syscall.h
@@ -4,6 +4,11 @@
 #include "mips.h"
 #include "mmu.h"
 
+#define SYSCALLNO(r) (r->regs[2])
+#define CALLARG(r,n) (r->regs[(n)+4])
+#define CALLSTATE(r) (r->regs[7])
+#define CALLRET(r) (r->regs[2])
+
 void sys_uname(struct mips_regs *, struct mmu *);
 void sys_brk(struct mips_regs *, struct mmu *);
 
