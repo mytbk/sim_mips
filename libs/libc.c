b59fde2797c84eeb373d69b579233fb538a6fd09
Iru Cai <mytbk920423@gmail.com>
2014-12-01 22:11:18 +0800

    more library and system calls

--- libs/libc.c
+++ libs/libc.c
@@ -1,4 +1,9 @@
+// assembly functions
 int write(int fd, char *s, int len);
+
+// for malloc()
+char buffer[1<<22];
+int nextfree=0;
 
 int strlen(char *s)
 {
@@ -14,4 +19,56 @@ int puts(char *s)
     write(1, "\n", 1);
     return l+1;
 }
+
+void* malloc(unsigned long size)
+{
+    void* ret = buffer+nextfree;
+    nextfree += (nextfree+size+4)&~3;
+    return ret;
+}
+
+void free(void* addr)
+{
+    return;
+}
+
+int strcmp(char *s, char *t)
+{
+    while (*s && *s==*t) {
+        s++;
+        t++;
+    }
+    return *s-*t;
+}
+
+char* strcpy(char *dst, const char *src)
+{
+    char *ret = dst;
+    while (*dst++=*src++)
+        ;
+    
+    return ret;
+}
+
+void *memcpy(char *dst, char *src, unsigned long len)
+{
+    void *ret = (void*)dst;
+    
+    while (len) {
+        *dst = *src;
+        src++;
+        dst++;
+        len--;
+    }
+    return ret;
+}
+
+
+
+
+
+
+
+
+
 
