67dbe467ec0523e2471818ccc731b5e967da1236
Iru Cai <mytbk920423@gmail.com>
2014-12-23 09:13:01 +0800

    test for union and strcpy in dhrystone

--- example/libtest/libtest.c
+++ example/libtest/libtest.c
@@ -1,11 +1,42 @@
 #include "libs.h"
 #include "printf.h"
 
+typedef int Enumeration;
+
+typedef struct record
+{
+    struct record *Ptr_Comp;
+    Enumeration    Discr;
+    union {
+        struct
+        {
+            Enumeration Enum_Comp;
+            int         Int_Comp;
+            char        Str_Comp [31];
+        } var_1;
+        struct
+        {
+            Enumeration E_Comp_2;
+            char        Str_2_Comp [31];
+        } var_2;
+        struct
+        {
+            char        Ch_1_Comp;
+            char        Ch_2_Comp;
+        } var_3;
+    } variant;
+} Rec_Type, *Rec_Pointer;
+
 int main(int argc, char *argv[])
 {
     char mystr[100];
     int (*scmp)(const char*, const char*);
     
+    Rec_Type *Ptr_Glob = (Rec_Type*) malloc (sizeof (Rec_Type));
+    strcpy (Ptr_Glob->variant.var_1.Str_Comp, 
+            "DHRY");
+    printf("%s\n", Ptr_Glob->variant.var_1.Str_Comp);
+
     fprintf(stdout, "argc=%d\n");
     
     if (argc>=0) {
