847def41ef749385818b5d9157644425f7c6e2bd
Iru Cai <mytbk920423@gmail.com>
2014-12-23 08:53:51 +0800

    fix debugger

--- mipsinst.c
+++ mipsinst.c
@@ -19,6 +19,7 @@ insttype_t get_inst_type(mipsinst_t inst)
 
 extern r_inst_tab mips_r_insts[];
 extern i_inst_tab mips_i_insts[];
+extern j_inst_tab mips_j_insts[];
 
 void
 print_disas_string(mipsinst_t inst)
@@ -26,37 +27,44 @@ print_disas_string(mipsinst_t inst)
     switch (get_inst_type(inst)) {
     case R_TYPE:
         if (inst.r.funct==MR_SLL || inst.r.funct==MR_SRL) {
-            log_msg(mips_r_insts[inst.r.funct].asmstr,
+            fprintf(stderr, mips_r_insts[inst.r.funct].asmstr,
                     inst.r.rd, inst.r.rt, inst.r.sa);
         } else {
-            log_msg(mips_r_insts[inst.r.funct].asmstr,
+            fprintf(stderr, mips_r_insts[inst.r.funct].asmstr,
                     inst.r.rd, inst.r.rs, inst.r.rt);
         }
         break;
     case I_TYPE:
         switch (inst.i.opcode) {
         case MI_SW:
+        case MI_SWL:
+        case MI_SWR:
         case MI_LW:
         case MI_SB:
+        case MI_SH:
         case MI_LB:
         case MI_LBU:
-            log_msg(mips_i_insts[inst.i.opcode].asmstr,
+        case MI_LHU:
+            fprintf(stderr, mips_i_insts[inst.i.opcode].asmstr,
                     inst.i.rt, inst.i.imm, inst.i.rs);
             break;
             
         default:
-            log_msg(mips_i_insts[inst.i.opcode].asmstr,
+            fprintf(stderr, mips_i_insts[inst.i.opcode].asmstr,
                     inst.i.rt, inst.i.rs, inst.i.imm);
             break;
         }
         
     case J_TYPE:
+        fprintf(stderr, mips_j_insts[inst.j.opcode].asmstr,
+                inst.j.imm);
+        break;
         
     default:
         // should not go here
         break;
     }
-    log_msg("\n");
+    fprintf(stderr, "\n");
 }
 
 
