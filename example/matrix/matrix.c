e147a7f53bd7f8632668152ae879fcbbabf600d6
Iru Cai <mytbk920423@gmail.com>
2015-01-10 19:32:18 +0800

    matrix program to 1 file

--- example/matrix/matrix.c
+++ example/matrix/matrix.c
@@ -1,11 +1,12 @@
 #define MSIZE 100
-static int A[MSIZE][MSIZE], B[MSIZE][MSIZE], C[MSIZE][MSIZE];
+int A[MSIZE][MSIZE], B[MSIZE][MSIZE], C[MSIZE][MSIZE];
 
 int main()
 {
     int i, j, k, nTimes;
 
     for (nTimes=0; nTimes<4; nTimes++) {
+#ifdef MATRIX01
         for (i=0; i<MSIZE; i++) {
             for (j=0; j<MSIZE; j++) {
                 int sum = 0;
@@ -15,6 +16,18 @@ int main()
                 C[i][j] = sum;
             }
         }
+#endif
+#ifdef MATRIX02
+        for (j=0; j<MSIZE; j++) {
+            for (i=0; i<MSIZE; i++) {
+                int sum = 0;
+                for (k=0; k<MSIZE; k++) {
+                    sum += A[i][k]*B[k][j];
+                }
+                C[i][j] = sum;
+            }
+        }
+#endif
     }
     
 }
