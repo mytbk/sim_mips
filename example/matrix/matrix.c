8f62087fe85e809393c427b6097abba379b06a08
Iru Cai <mytbk920423@gmail.com>
2015-01-10 20:11:27 +0800

    matrix03

--- example/matrix/matrix.c
+++ example/matrix/matrix.c
@@ -28,6 +28,23 @@ int main()
             }
         }
 #endif
+#ifdef MATRIX03
+        for (i=0; i<MSIZE; i+=2) {
+            for (j=0; j<MSIZE; j+=2) {
+                int a=0,b=0,c=0,d=0;
+                for (k=0; k<MSIZE; k+=2) {
+                    a += A[i][k]*B[k][j] + A[i][k+1]*B[k+1][j];
+                    b += A[i][k]*B[k][j+1] + A[i][k+1]*B[k+1][j+1];
+                    c += A[i+1][k]*B[k][j] + A[i+1][k+1]*B[k+1][j];
+                    d += A[i+1][k]*B[k][j+1] + A[i+1][k+1]*B[k+1][j+1];
+                }
+                C[i][j] = a;
+                C[i][j+1] = b;
+                C[i+1][j] = c;
+                C[i+1][j+1] = d;
+            }
+        }
+#endif
     }
     
 }
