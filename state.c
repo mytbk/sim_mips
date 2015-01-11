c880c6156b09b8b8e1647e415cb5f917085eab69
Iru Cai <mytbk920423@gmail.com>
2015-01-11 21:05:14 +0800

    add percentage to state

--- state.c
+++ state.c
@@ -8,15 +8,17 @@ state_t State = {};
 void
 state_print()
 {
+    unsigned nInst = State.r_inst+State.i_inst+State.j_inst;
+    
     log_msg("### Instructions ###\n");
-    log_msg("R-type:  %d\n", State.r_inst);
-    log_msg("I-type:  %d\n", State.i_inst);
-    log_msg("J-type:  %d\n", State.j_inst);
-    log_msg("Load:    %d\n", State.nLoad);
-    log_msg("Store:   %d\n", State.nStore);
-    log_msg("Jump:    %d\n", State.nJump);
-    log_msg("Branch:  %d\n", State.nBranch);
-    log_msg("Syscall: %d\n", State.nSyscall);
+    log_msg("R-type:  %d (%lf%%)\n", State.r_inst, 100.0*State.r_inst/nInst);
+    log_msg("I-type:  %d (%lf%%)\n", State.i_inst, 100.0*State.i_inst/nInst);
+    log_msg("J-type:  %d (%lf%%)\n", State.j_inst, 100.0*State.j_inst/nInst);
+    log_msg("Load:    %d (%lf%%)\n", State.nLoad, 100.0*State.nLoad/nInst);
+    log_msg("Store:   %d (%lf%%)\n", State.nStore, 100.0*State.nStore/nInst);
+    log_msg("Jump:    %d (%lf%%)\n", State.nJump, 100.0*State.nJump/nInst);
+    log_msg("Branch:  %d (%lf%%)\n", State.nBranch, 100.0*State.nBranch/nInst);
+    log_msg("Syscall: %d (%lf%%)\n", State.nSyscall, 100.0*State.nSyscall/nInst);
     log_msg("\n### Cache ###\n");
     log_msg("D-Cache hit:  %d\n", sim_dcache.nHit);
     log_msg("D-Cache miss: %d\n", sim_dcache.nMiss);
